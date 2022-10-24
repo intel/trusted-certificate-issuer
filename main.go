3fa5a54d4261efb60bb30f74ba48b64ed1013645
Amarnath Valluri <amarnath.valluri@intel.com>
2022-10-24 10:37:41 +0300

    bring back the 'use-random-nonce' commandline argument
    Which was accidentally removed in the earlier commit

--- main.go
+++ main.go
@@ -66,6 +66,7 @@ func main() {
 	flag.StringVar(&cfg.HSMUserPin, "user-pin", "", "PKCS11 token user pin.")
 	flag.StringVar(&cfg.HSMSoPin, "so-pin", "", "PKCS11 token so/admin pin.")
 	flag.BoolVar(&cfg.CSRFullCertChain, "csr-full-cert-chain", false, "Return full certificate chain in Kubernetes CSR certificate.")
+	flag.BoolVar(&cfg.RandomNonce, "use-random-nonce", true, "Use random nonce for SGX quote generation. Needed for KMRA version >= v2.2.")
 	flag.StringVar(&cfg.KeyWrapMechanism, "key-wrap-mechanism", config.KeyWrapAesKeyWrapPad, "CA private key wrapping mechanism to use. One of: 'aes_gcm' or 'ads_key_pad_wrap' ")
 
 	opts := zap.Options{}
